072b0f021fac9c758229b3dd9e33fd9bea047966
Claude A. Aboke <abokeclaude4@gmail.com>
2026-04-26 18:26:32 -0600

    Delete src/com/aboke/FirstProgram.java

--- src/com/aboke/FirstProgram.java
+++
@@ -1,8 +0,0 @@
-package com.aboke;
-
-public class FirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello Claude");
-        System.out.println("Welcome to Java");
-    }
-}
